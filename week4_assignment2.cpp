b29fde83739538263e3c4a205f9cb825bd6f8e78
Anshal Jain <93059296+pknessness@users.noreply.github.com>
2025-01-28 18:26:52 -0800

    Update week4_assignment2.cpp
    changed assignment 2 to base code example

--- week4_assignment2.cpp
+++ week4_assignment2.cpp
@@ -1,214 +1,60 @@
 //STARTER 
-//THIS EXERCISE IS TO BE DONE IN A GENERIC C++ COMPILER
-//https://www.programiz.com/cpp-programming/online-compiler/
-//IF YOU HAVE ISSUES WITH THE COMPILER, CONTACT EMBEDDED LEAD
+//THIS EXERCISE IS TO BE DONE AND THEN RUN IN REAL LIFE ON A ROBOT
+//YOU WILL NOT BE ABLE TO COMPILE THIS UNLESS YOU SET UP THE BUILD ENVIRONMENT
+#include "main.h"
 
-// super simplified version of how DJIMotor works and how we access the remote data
-#include <string>
-#include <cstdint>
-#include <iostream>
-constexpr int CAN_HANDLER_NUMBER = 2;
+DigitalOut led(L26);
+DigitalOut led2(L27);
+DigitalOut led3(L25);
 
-enum motorType {
-    NONE = 0,
-    STANDARD = 1,
+I2C i2c(I2C_SDA, I2C_SCL);
 
-    GIMBLY = 2,
-    GM6020 = 2,
-
-    M3508 = 3,
-
-    C610 = 4,
-    M2006 = 4,
-
-    M3508_FLYWHEEL = 5,
-};
-
-enum motorDataType {
-    ANGLE,
-    VELOCITY,
-    TORQUE,
-    TEMPERATURE,
-    POWER,
-};
-
-class Remote {
-public:
-
-    
-
-enum class SwitchState
-{
-    UNKNOWN,
-    DOWN,
-    MID,
-    UP
-};
-
-    int leftX(){
-        return lefX;
-    }
-
-    int leftY(){
-        return lefY;
-    }
-
-    int rightX(){
-        return righX;
-    }
-
-    int rightY(){
-        return righY;
-    }
-
-    Remote::SwitchState leftSwitch(){
-        return lefS;
-    }
-
-    Remote::SwitchState rightSwitch(){
-        return righS;
-    }
-
-    void read(bool debug = false){
-        static int count;
-        count %= 660;
-
-        if((count % 15) / 5  == 0)
-            lefS = Remote::SwitchState::UP;
-
-        else if((count % 15) / 5 == 1)
-            lefS = Remote::SwitchState::MID;
-
-        else if((count % 15) / 5 == 2)
-            lefS = Remote::SwitchState::DOWN;
-
-        lefX = count;
-
-        if(debug){
-            printf("lX:[%d] lS:[%d]",lefX, (int)lefS);
-        }
-
-        count++;
-    }
-
-private:
-    int lefX, lefY, righX, righY;
-    Remote::SwitchState lefS, righS;
-
-};
-
-class CANHandler{
-    public:
-        enum CANBus {CANBUS_1, CANBUS_2, NOBUS};
-};
-
-class DJIMotor{
-
-private:
-    static DJIMotor* s_allMotors  [CAN_HANDLER_NUMBER][3][4];
-    static bool s_motorsExist     [CAN_HANDLER_NUMBER][3][4];
-
-    short motorID_0;
-    short canID_0;
-    motorType type;
-    CANHandler::CANBus canBus;
-    std::string name;
-
-    enum motorMoveMode{
-        OFF,
-        POS,
-        SPD,
-        POW,
-        ERR
-    };
-
-    int value = 0;
-    motorMoveMode mode = OFF;
-
-    int16_t motorData[5] = {};
-
-public:
-    DJIMotor(short motorID, CANHandler::CANBus canBus, motorType type, const std::string& name) {
-        canID_0 = static_cast<short>(motorID - 1);
-        motorID_0 = canID_0;
-        this -> canBus = canBus;
-        this -> type = type;
-        this -> name = name;
-
-        if(type == GM6020)
-            canID_0 += 4;
-
-        s_allMotors[canBus][canID_0 / 4][canID_0 % 4] = this;
-        s_motorsExist[canBus][canID_0 / 4][canID_0 % 4] = true;
-    }
-
-    void setPower(int power){
-        mode = POW;
-        value = power;
-    }
-
-    void setSpeed(int speed){
-        mode = SPD;
-        value = speed;
-    }
-
-    void setPosition(int position){
-        mode = POS;
-        value = position;
-    }
-
-    void setOutput(){
-        if(mode == POW)
-            motorData[POWER] = static_cast<int16_t>(value);
-
-        else if(mode == SPD)
-            motorData[VELOCITY] = static_cast<int16_t>(value);
-
-        else if(mode == POS)
-            motorData[ANGLE] = static_cast<int16_t>(value);
-    }
-
-    static void s_sendValues(){
-        for(int canBus = 0; canBus < CAN_HANDLER_NUMBER; canBus ++)
-            for(int j = 0; j < 3; j ++)
-                for(int k = 0; k < 4; k ++)
-                    if(s_motorsExist[canBus][j][k])
-                        s_allMotors[canBus][j][k] -> setOutput();
-
-    }
-
-    static void s_getFeedback(){
-        // need for PID in real class
-    }
-
-    int getData(motorDataType data){
-        return motorData[data];
-    }
-};
-
-DJIMotor* DJIMotor::s_allMotors[2][3][4];
-bool DJIMotor::s_motorsExist[2][3][4];
-Remote remote;
-
-void remoteRead(bool debug = false){
-    remote.read(debug);
-}
-
-///////////////
-//main.cpp
-///////////////
-
-
-// ONLY ADD CODE BELOW
-// Use prints and getData() for debugging purposes
+//DEFINE MOTORS, ETC
 
 int main(){
-    
-    //SETUP CODE HERE
+
+    //assigning can handler objects to motor class.
+    DJIMotor::s_setCANHandlers(&canHandler1,&canHandler2, false, false); 
+
+    //getting initial feedback.
+    DJIMotor::s_getFeedback();
+
+    unsigned long loopTimer_u = us_ticker_read();
+    unsigned long timeEnd_u;
+    unsigned long timeStart_u;
+
+    int refLoop = 0;
+
+    //DEFINE PIDs AND OTHER CONSTANTS
 
     while(true){ //main loop
+        timeStart_u = us_ticker_read();
 
-        //MAIN CODE HERE
+        //inner loop runs every 25ms
+        if((timeStart_u - loopTimer_u) / 1000 > 25) { 
+            loopTimer_u = timeStart_u;
+            led = !led; //led blink tells us how fast the inner loop is running
 
+            if (refLoop >= 5) { //ref code runs 5 of every inner loop, 
+                refLoop = 0;
+                refereeThread(&referee);
+            }
+            refLoop ++;
+
+            remoteRead(); //reading data from remote
+
+            //MAIN CODE 
+            //MOST CODE DOESNT NEED TO RUN FASTER THAN EVERY 25ms
+
+            timeEnd_u = us_ticker_read();
+
+            DJIMotor::s_sendValues();
+        }
+
+        //FEEDBACK CODE DOES NEED TO RUN FASTER THAN 1MS
+        //OTHER QUICK AND URGENT TASKS GO HERE
+
+        DJIMotor::s_getFeedback();
+        ThisThread::sleep_for(1ms);
     }
 }
